94aecb9315d568d194d42e7493ac4bb08f1b7c6d
Anthony Chivetta <anthony@chivetta.org>
2010-12-27 01:18:43 -0500

    Removed all hash table resizing code.
    This code needs a lot of work to make work in the kernel.  I'd rather it not be
    around while we try to build multi-threading into the code.  We'll add this
    feature back from scratch after that's done.

--- storage.c
+++ storage.c
@@ -24,10 +24,6 @@
 #include "storage.h"
 #include "hash.h"
 
-#ifdef ASSOC_SUPPORT_EXPAND
-static pthread_cond_t maintenance_cond = PTHREAD_COND_INITIALIZER;
-#endif
-
 typedef  unsigned long  int  ub4;   /* unsigned 4-byte quantities */
 typedef  unsigned       char ub1;   /* unsigned 1-byte quantities */
 
@@ -44,31 +40,8 @@ static unsigned int hashpower = 18;
 /* Main hash table. This is where we look except during expansion. */
 static item_t** primary_hashtable = 0;
 
-#ifdef ASSOC_SUPPORT_EXPAND
-/*
- * Previous hash table. During expansion, we look here for keys that haven't
- * been moved over to the primary yet.
- */
-static item_t** old_hashtable = 0;
-#endif
-
 /* Number of items in the hash table. */
 static unsigned int hash_items = 0;
-
-#ifdef ASSOC_SUPPORT_EXPAND
-/* Flag: Are we in the middle of expanding now? */
-static bool expanding = false;
-
-/*
- * During expansion we migrate values with bucket granularity; this is how
- * far we've gotten so far. Ranges from 0 .. hashsize(hashpower - 1) - 1.
- */
-static unsigned int expand_bucket = 0;
-
-void do_assoc_move_next_bucket(void);
-int start_assoc_maintenance_thread(void);
-void stop_assoc_maintenance_thread(void);
-#endif
 
 static uint64_t cas;
 static void update_cas(item_t* item)
@@ -146,22 +119,9 @@ item_t* create_item(const char* key, size_t nkey, const char* data,
 item_t *get_item(const char *key, const size_t nkey) {
     uint32_t hv = hash(key, nkey, 0);
     item_t *it, *ret = NULL;
-#ifdef ASSOC_SUPPORT_EXPAND
-    unsigned int oldbucket;
-#endif
     int depth = 0;
 
-#ifdef ASSOC_SUPPORT_EXPAND
-    if (expanding &&
-        (oldbucket = (hv & hashmask(hashpower - 1))) >= expand_bucket)
-    {
-        it = old_hashtable[oldbucket];
-    } else {
-        it = primary_hashtable[hv & hashmask(hashpower)];
-    }
-#else
     it = primary_hashtable[hv & hashmask(hashpower)];
-#endif
 
     while (it) {
         if ((nkey == it->nkey) && (memcmp(key, it->key, nkey) == 0)) {
@@ -179,19 +139,8 @@ item_t *get_item(const char *key, const size_t nkey) {
 static item_t** _hashitem_before (const char *key, const size_t nkey) {
     uint32_t hv = hash(key, nkey, 0);
     item_t **pos;
-#ifdef ASSOC_SUPPORT_EXPAND
-    unsigned int oldbucket;
 
-    if (expanding &&
-        (oldbucket = (hv & hashmask(hashpower - 1))) >= expand_bucket)
-    {
-        pos = &old_hashtable[oldbucket];
-    } else {
-        pos = &primary_hashtable[hv & hashmask(hashpower)];
-    }
-#else
     pos = &primary_hashtable[hv & hashmask(hashpower)];
-#endif
 
     while (*pos && ((nkey != (*pos)->nkey) || memcmp(key, (*pos)->key, nkey))) {
         pos = &(*pos)->h_next;
@@ -219,9 +168,6 @@ bool delete_item(const char *key, const size_t nkey) {
 /* Note: this isn't an update.  The key must not already exist to call this */
 void put_item(item_t *it) {
     uint32_t hv;
-#ifdef ASSOC_SUPPORT_EXPAND
-    unsigned int oldbucket;
-#endif
 
 #ifndef NDEBUG
     BUG_ON(get_item(it->key, it->nkey) != 0);  /* shouldn't have duplicately named things defined */
@@ -231,27 +177,10 @@ void put_item(item_t *it) {
 
     hv = hash(it->key, it->nkey, 0);
 
-#ifdef ASSOC_SUPPORT_EXPAND
-    if (expanding &&
-        (oldbucket = (hv & hashmask(hashpower - 1))) >= expand_bucket)
-    {
-        it->h_next = old_hashtable[oldbucket];
-        old_hashtable[oldbucket] = it;
-    } else {
-        it->h_next = primary_hashtable[hv & hashmask(hashpower)];
-        primary_hashtable[hv & hashmask(hashpower)] = it;
-    }
-#else
     it->h_next = primary_hashtable[hv & hashmask(hashpower)];
     primary_hashtable[hv & hashmask(hashpower)] = it;
-#endif
 
     hash_items++;
-#ifdef ASSOC_SUPPORT_EXPAND
-    if (! expanding && hash_items > (hashsize(hashpower) * 3) / 2) {
-        assoc_expand();
-    }
-#endif
 }
 
 /** Release all locks on an item
@@ -296,104 +225,3 @@ void flush(uint32_t when){
         primary_hashtable[i] = NULL;
     }
 }
-
-#ifdef ASSOC_SUPPORT_EXPAND
-/***************************************************************************
- * HERE LIES THINGS THAT INCREASE THE SIZE OF THE HASH SPACE               *
- ***************************************************************************/
-
-/* grows the hashtable to the next power of 2. */
-static void assoc_expand(void) {
-    old_hashtable = primary_hashtable;
-
-    primary_hashtable = calloc(hashsize(hashpower + 1), sizeof(void *));
-    if (primary_hashtable) {
-        if (settings.verbose > 1)
-            fprintf(stderr, "Hash table expansion starting\n");
-        hashpower++;
-        expanding = true;
-        expand_bucket = 0;
-        pthread_cond_signal(&maintenance_cond);
-    } else {
-        primary_hashtable = old_hashtable;
-        /* Bad news, but we can keep running. */
-    }
-}
-
-static volatile int do_run_maintenance_thread = 1;
-
-#define DEFAULT_HASH_BULK_MOVE 1
-int hash_bulk_move = DEFAULT_HASH_BULK_MOVE;
-
-static void *assoc_maintenance_thread(void *arg) {
-
-    while (do_run_maintenance_thread) {
-        int ii = 0;
-
-        /* Lock the cache, and bulk move multiple buckets to the new
-         * hash table. */
-        pthread_mutex_lock(&cache_lock);
-
-        for (ii = 0; ii < hash_bulk_move && expanding; ++ii) {
-            item_t *it, *next;
-            int bucket;
-
-            for (it = old_hashtable[expand_bucket]; NULL != it; it = next) {
-                next = it->h_next;
-
-                bucket = hash(ITEM_key(it), it->nkey, 0) & hashmask(hashpower);
-                it->h_next = primary_hashtable[bucket];
-                primary_hashtable[bucket] = it;
-            }
-
-            old_hashtable[expand_bucket] = NULL;
-
-            expand_bucket++;
-            if (expand_bucket == hashsize(hashpower - 1)) {
-                expanding = false;
-                free(old_hashtable);
-                if (settings.verbose > 1)
-                    fprintf(stderr, "Hash table expansion done\n");
-            }
-        }
-
-        if (!expanding) {
-            /* We are done expanding.. just wait for next invocation */
-            pthread_cond_wait(&maintenance_cond, &cache_lock);
-        }
-
-        pthread_mutex_unlock(&cache_lock);
-    }
-    return NULL;
-}
-
-static pthread_t maintenance_tid;
-
-int start_assoc_maintenance_thread() {
-    int ret;
-    char *env = getenv("MEMCACHED_HASH_BULK_MOVE");
-    if (env != NULL) {
-        hash_bulk_move = atoi(env);
-        if (hash_bulk_move == 0) {
-            hash_bulk_move = DEFAULT_HASH_BULK_MOVE;
-        }
-    }
-    if ((ret = pthread_create(&maintenance_tid, NULL,
-                              assoc_maintenance_thread, NULL)) != 0) {
-        fprintf(stderr, "Can't create thread: %s\n", strerror(ret));
-        return -1;
-    }
-    return 0;
-}
-
-void stop_assoc_maintenance_thread() {
-    pthread_mutex_lock(&cache_lock);
-    do_run_maintenance_thread = 0;
-    pthread_cond_signal(&maintenance_cond);
-    pthread_mutex_unlock(&cache_lock);
-
-    /* Wait for the maintenance thread to stop */
-    pthread_join(maintenance_tid, NULL);
-}
-
-#endif
