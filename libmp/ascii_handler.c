bdda3b9ad3fb87f01160cf5cf018fc74b2c3b3ef
INADA Naoki <inada-n@klab.com>
2012-10-30 01:16:30 +0900

    reduce stack usage.

--- libmp/ascii_handler.c
+++ libmp/ascii_handler.c
@@ -130,13 +130,11 @@ ascii_get_response_handler(const void *cookie,
                            uint64_t cas)
 {
   memcached_protocol_client_st *client= (void*)cookie;
-  char buffer[300];
+  char buffer[60] = "VALUE ";
   const char *source= key;
   char *dest= buffer + 6;
   int x;
   size_t used;
-
-  strcpy(buffer, "VALUE ");
 
   for (x = 0; x < keylen; ++x)
   {
